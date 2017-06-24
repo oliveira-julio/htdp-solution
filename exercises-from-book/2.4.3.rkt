cf83ffc7fa4a2b907c6195f398eef071916c7bb4
Julio Oliveira <antoniojuliodeo@gmail.com>
2017-06-24 01:27:02 -0300

    2.4.3

---
+++ exercises-from-book/2.4.3.rkt
@@ -0,0 +1,27 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname 2.4.3) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;;(+ 5 (/ 1 0))
+;; ERROR:
+;;  /: division by zero
+;;
+;; MEANING:
+;;  There is an operation trying to divide a number by zero.
+
+;;(sin 10 20)
+;; ERROR:
+;;  sin: expects only 1 argument, but found 2
+;;
+;; MEANING:
+;;  Operator 'sin' receives more arguments than it was designed to receive
+
+
+;;(somef 10)
+;; ERROR:
+;;  somef: this function is not defined
+;;
+;; MEANING:
+;;  the function 'sumif' was not defined previously
+
+
+
