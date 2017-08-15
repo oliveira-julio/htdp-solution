aefd41f7e4c07de00d0455a4d96d59b9c880b3b9
Julio Oliveira <antoniojuliodeo@gmail.com>
2017-08-14 23:33:24 -0300

    4.1.1

---
+++ exercises-from-book/I/section_4/4.1.1.rkt
@@ -0,0 +1,15 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname 4.1.1) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+
+
+;; true
+(and (> 4 3) (<= 10 100))
+
+
+;; true
+(or (> 4 3) (= 10 100))
+
+
+;; true
+(not (= 2 3 ))
